a8c6f8ecfe8417d5bfcd2973f26fbaebb8cc7ddc
kelco-chan <60736592+kelco-chan@users.noreply.github.com>
2020-04-29 15:23:41 +1000

    major change in object cb

--- hw.js
+++ hw.js
@@ -1,10 +1,37 @@
+
+//HOMEWORK REVIEW - Kelvin Chan
+
 let prefix ="$"
 var hwentries=[];
 var discord=require("discord.js");
 var bot = new discord.Client("");
+let commands={};
+commands["homework"]=function(message,args){
+	if(!(args[0]&&args[1]])){
+		return "Give me something VALID to add. I thought bots were supposed to be more stupid than you.";
+	}
+	hwentries.push(new Homework(args[0],args[1],args.slice(2).join(" ")));
+	console.log(hwentries);
+	return hwentries[hwentries.length-1].toString();
+}
+commands["test"]=function(message,args){
+	return "Beep Boop. Boop Beep?"
+};
+commands["list"]=function(message,args){
+	var prnt="";
+	for(var i=0;i<hwentries.length;i++){
+		prnt+="\nEntry "+(i+1)+": \n"+hwentries[i].toString()+" \n";
+	}
+	return prnt;
+}
+commands["change-prefix"]=function(message,args){
+	prefix=args[0];
+	return `Prefix changed to ${prefix}`
+}
 bot.on("ready", async ()=> {
 	console.log("Bot is ready");
-})
+});
+
 class Homework {
 	constructor(type,due,description){
 		this.type=type;
@@ -20,66 +47,19 @@ bot.on("message", async message => {
 	if((message.author.bot)||(message.channel.type==="dm")){
 		return;
 	}
-	var output="";
+	//
+	var output;
 	if (message.content.substring(0,prefix.length)===prefix){
-		let command = message.content.split(" ")[0];
+		let command = message.content.split(" ")[0].substring(prefix.length);
 		let args = message.content.split(" ").slice(1);
-		if(command==(prefix+"test")){
-			output="prefix detected. Beep Boop. Boop beep?";
-		} 
-		if(command==(prefix+"homework")){
-			hwentries.push(new Homework(args[0],args[1],args.slice(2).join(" ")));
-			console.log(hwentries);
-			output=hwentries[hwentries.length-1].toString();
-		}
-		if(command===(prefix+"list")){
-
-			for(var i=0;i<hwentries.length;i++){
-				output+="\nEntry "+(i+1)+": \n"+hwentries[i].toString()+" \n";
-			}
-			console.log(output);
-		}
-		if(command ===(prefix+"help")){
-			output="Commands:\nhelp\nlist\nhomework\ntest\n";
-		}
-		if(command==(prefix+"change-prefix")){
-			//if(message.member.roles.has(704540113719263343)){// only admins
-				prefix=args[0];
-				output=`updated prefix to ${prefix}`
-			//}else{
-			//	output="NO U";
-			//}
-			
+		if (commands.hasOwnProperty(command)){
+			output=commands[command](message,args);
 		}
 		//logging output
-		if(output!==""){
+		if(output){
 			return message.channel.send(output);
 		}
 		
 	}
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//DO NOT READ
-
-
-
-
-
-
-
+});
 bot.login(process.env.BOT_TOKEN);
