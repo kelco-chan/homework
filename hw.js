b6fa431431b9f94b4d0f36daf096d14545be8ff7
kelco-chan <60736592+kelco-chan@users.noreply.github.com>
2020-04-30 14:05:09 +1000

    Update hw.js

--- hw.js
+++ hw.js
@@ -45,7 +45,7 @@ commands["list"]=function(message,args){
 	console.log("loopstart");
 	console.log(hwentries);
 	for(j in hwentries){
-		prnt+=`\n Homework ${j.id} :\n ${j.toString()} \n`;
+		prnt+=`\n Homework ${hwentries[j].id} :\n ${hwentries[j].toString()} \n`;
 		console.log("looping")
 	}
 	console.log("loopend");
